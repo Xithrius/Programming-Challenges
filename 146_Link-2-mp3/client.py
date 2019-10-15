1e32575024afc7f0d3e7cddc208c43e1f34450f1
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-10-15 00:13:10 -0700

    moved config to .py file

--- 146_Link-2-mp3/client.py
+++ 146_Link-2-mp3/client.py
@@ -1,10 +1,20 @@
 import youtube_dl
-import json
 import sys
 
 
-with open('config.json') as f:
-    data = json.load(f)
+data = {
+    "format": "bestaudio/best",
+    "outtmpl": "output/%(title)s.mp3",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": False,
+    "no_warnings": False,
+    "default_search": "auto",
+    "source_address": "0.0.0.0"
+}
 
 ytdl = youtube_dl.YoutubeDL(data)
 ytdl.extract_info(sys.argv[1], download=True)
